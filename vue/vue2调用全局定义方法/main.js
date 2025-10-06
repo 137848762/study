65b31abb25f5e98fd45e80b2c99e65d31e8b46d8
137848762 <90426707+137848762@users.noreply.github.com>
2025-10-07 04:37:42 +0800

    Update main.js

--- vue/vue2调用全局定义方法/main.js
+++ vue/vue2调用全局定义方法/main.js
@@ -1,7 +1,27 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-const app = createApp(App)
-app.config.globalProperties.$myGlobalMethod = function(){
-	console.log("这是vue3调用全局方法")
+// 引入全局方法插件
+import globalMethods from './plugins/globalMethods.js'
+
+Vue.config.productionTip = false
+
+// 1. 通过 Vue.prototype 定义全局方法
+Vue.prototype.$myGlobalMethod = function(){
+	console.log("这是一个全局方法")
 }
-app.mount('#app')
+
+// 2. 通过 Vue.mixin() 定义全局混入方法
+Vue.mixin({
+  methods: {
+    $anotherGlobalMethod() {
+      console.log("这是通过混入定义的全局方法");
+    }
+  }
+})
+
+// 3. 使用插件定义全局方法
+Vue.use(globalMethods)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
