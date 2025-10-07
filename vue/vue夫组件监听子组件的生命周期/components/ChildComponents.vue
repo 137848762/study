9a98c79d3e2226117be10d7fad8f94c6539a1a96
137848762 <90426707+137848762@users.noreply.github.com>
2025-10-07 10:43:37 +0800

    Create ChildComponents.vue

---
+++ vue/vue夫组件监听子组件的生命周期/components/ChildComponents.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+	<div>子组件1</div>
+</template>
+<script>
+	export default {
+		name:"ChildComponent",
+		mounted() {
+			this.$emit('childmounted');
+		}
+	}
+</script>
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
