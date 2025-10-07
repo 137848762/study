64c921092a653d6c0d73e1a4ff618fef96577aaf
137848762 <90426707+137848762@users.noreply.github.com>
2025-10-07 10:44:07 +0800

    Add files via upload

---
+++ vue/vue夫组件监听子组件的生命周期/components/childComponent2.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>子组件2</div>
+</template>
+
+<script>
+	export default{
+		name:"childComponent2",
+		mounted() {
+			console.log("子组件2已经挂载");
+		}
+		
+	}
+</script>
+
+<style>
+</style>
---
+++ vue/vue夫组件监听子组件的生命周期/components/childComponent3.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		子组件3
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"ChildComponent3",
+		inject:['notifyParent'],
+		mounted() {
+			this.notifyParent('mounted');
+		}
+		
+	}
+</script>
+
+<style>
+</style>
